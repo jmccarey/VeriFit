d9013018ec06398154c1ba997c138c65b793caea
jmccarey <joseph@mccarey.net>
2019-12-08 16:35:59 -0600

    removed tempdirs for sessions

--- application.py
+++ application.py
@@ -22,7 +22,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
