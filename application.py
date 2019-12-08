4e8fb51187498e1322648df53524e1b1210ce171
jmccarey <joseph@mccarey.net>
2019-12-08 15:27:44 -0600

    Added procfile

--- application.py
+++ application.py
@@ -1,30 +1,19 @@
 import os
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, make_response
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from utils import *
-import pyrebase
+from utils import login_required, get_goals, get_menu, reformat, add_items, get_entry, get_total, update, get_options, update_options, update_goals
 import re
 import requests
 from datetime import date as d
+import sqlite3
 
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
-config = {
-  "apiKey": "AIzaSyD1OYOIIDK1ZXTFDs-ykrQwo3baTouP1pU",
-  "authDomain": "verifit-99eee.firebaseapp.com",
-  "databaseURL": "https://verifit-99eee.firebaseio.com",
-  "storageBucket": "verifit-99eee.appspot.com"
-}
-
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
-db = firebase.database()
 
 @app.after_request
 def after_request(response):
@@ -42,15 +31,16 @@ Session(app)
 def login():
     session.clear()
     if request.method == "POST":
+        conn = sqlite3.connect("verifit.db")
+        db = conn.cursor()
         email = request.form.get("email")
         password = request.form.get("password")
         if email and password:
-            try:
-                user = auth.sign_in_with_email_and_password(email,password)
-                session['user_id'] = user["idToken"]
-            except:
+            db.execute("Select * from users where email=?", (email,))
+            user = db.fetchone()
+            if not user or not check_password_hash(user[2], password):
                 return render_template("login.html", error="Invalid email or password")
-            
+            session['user_id'] = user[0]
         return redirect('/')
     else:
         return render_template("login.html")
@@ -59,27 +49,128 @@ def login():
 def index():
     if session.get("user_id"):
         if request.method == "GET":
-            date = d.today().strftime("mm-dd-yyyy")
+            date = d.today().strftime("%m-%d-%y")
             menu = {
                 "breakfast": get_menu(date, 0),
                 "lunch": get_menu(date, 1),
                 "dinner": get_menu(date, 2)
             }
-            return render_template("index.html", date="Today", menu=menu)
+            breakfastEntry = get_entry("breakfast", session.get("user_id"), date)
+            lunchEntry = get_entry("lunch", session.get("user_id"), date)
+            dinnerEntry = get_entry("dinner", session.get("user_id"), date)
+            totals = {
+            "b": get_total(breakfastEntry),
+            "l": get_total(lunchEntry),
+            "d": get_total(dinnerEntry)
+            }
+            goal = get_goals(session.get("user_id"))["calories"]
+            resp = make_response(render_template("index.html", date=date, menu=menu, breakfast=breakfastEntry, lunch=lunchEntry, dinner=dinnerEntry, totals=totals, goal=goal))
+            resp.set_cookie("date", date)
+            return resp
         else:
             date = reformat(request.form.get("date"))
+            if not date:
+                date = request.cookies.get("date")
+                if not date:
+                    date = d.today().strftime("%m-%d-%y")
+                items = []
+                edit = False
+                for item in request.form.items():
+                    if len(item) > 1 and item[1] != '':
+                        items.append(item)
+                    elif item[0] == "breakfast" or item[0] == "lunch" or item[0] == "dinner":
+                        edit = False
+                        meal = item[0]
+                    elif item[0] == "bEdit":
+                        edit = True
+                        meal = "breakfast"
+                    elif item[0] == "lEdit":
+                        edit = True
+                        meal = "lunch"
+                    elif item[0] == "dEdit":
+                        edit = True
+                        meal = "dinner"
+                if edit:
+                    update(meal, items, session.get("user_id"), date)
+                else:
+                    add_items(meal, items, session.get("user_id"), date)
             menu = {
                 "breakfast": get_menu(date, 0),
                 "lunch": get_menu(date, 1),
                 "dinner": get_menu(date, 2)
             }
-            return render_template("index.html", date=date, menu=menu)
+            breakfastEntry = get_entry("breakfast", session.get("user_id"), date)
+            lunchEntry = get_entry("lunch", session.get("user_id"), date)
+            dinnerEntry = get_entry("dinner", session.get("user_id"), date)
+            totals = {
+            "b": get_total(breakfastEntry),
+            "l": get_total(lunchEntry),
+            "d": get_total(dinnerEntry)
+            }
+            goal = get_goals(session.get("user_id"))["calories"]
+            resp = make_response(render_template("index.html", date=date, menu=menu, breakfast=breakfastEntry, lunch=lunchEntry, dinner=dinnerEntry, totals=totals, goal=goal))
+            resp.set_cookie("date", date)
+            return resp
     else:
         return render_template("welcome.html")
+
+@app.route("/settings", methods=["GET", "POST"])
+@login_required
+def settings():
+    if request.method == "POST":
+        opts= get_options(session.get('user_id'))
+        name = request.form.get("name")
+        email = request.form.get("email")
+        old = request.form.get("password")
+        new = request.form.get("new")
+        confirm = request.form.get('confirm')
+        newOpts = {}
+        if check_password_hash(opts["hash"], old):
+            if new != '':
+                if not re.match(r"[\w@!.~#]{6,128}",new):
+                    return render_template("settings.html", opts=opts, error="Passwords must be 6+ characters and contain only alphanumeric characters or @!.~#")
+                elif not confirm == new:
+                    return render_template("settings.html", opts=opts, error="Passwords do not match")
+                else:
+                    newOpts["hash"] = generate_password_hash(new)
+            else:
+                newOpts["hash"] = opts["hash"]
+            if email != '':
+                newOpts["email"] = email
+            else:
+                newOpts["email"] = opts["email"]
+            if name != '':
+                newOpts["name"] = name
+            else:
+                name = opts["name"]
+            print(newOpts)
+            update_options(session.get('user_id'), newOpts)
+            return redirect("/settings")
+        else:
+            return render_template("settings.html", opts=opts, error="Old password incorrect")
+    else:
+        opts= get_options(session.get('user_id'))
+        return render_template("settings.html", opts=opts)
+
+@app.route("/goals", methods=["GET", "POST"])
+@login_required
+def goals():
+    if request.method == "POST":
+        currWeight = request.form.get("currWeight")
+        goalWeight = request.form.get("goalWeight")
+        calories = request.form.get("calories")
+        update_goals(session.get("user_id"), currWeight, goalWeight, calories)
+        return redirect("/goals")
+    else:
+        goals = get_goals(session.get("user_id"))
+        return render_template("goals.html", goals=goals)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        conn = sqlite3.connect("verifit.db")
+        db = conn.cursor()
+        name = request.form.get("name")
         email = request.form.get("email")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
@@ -88,12 +179,12 @@ def register():
                 return render_template("register.html", error="Passwords must be 6+ characters and contain only alphanumeric characters or @!.~#")
             if not password == confirmation:
                 return render_template("register.html", error="Password and confirmation do not match")
-        try:
-            user = auth.create_user_with_email_and_password(email, password)
-        except requests.exceptions.HTTPError:
+        db.execute("Select * from users where email=?;", (email,))
+        if db.fetchone():
             return render_template("register.html", error="Email in use")
-        token = user["idToken"]
-        auth.send_email_verification(token)
+        hsh = generate_password_hash(password)
+        db.execute("Insert into users (email, hash, name) values (?, ?, ?);", (email, hsh, name))
+        conn.commit()
         return redirect("/")
         
     else:
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import requests
 from lxml import html
 from lxml.cssselect import CSSSelector
 import re
+import sqlite3
 
 def login_required(f):
     @wraps(f)
@@ -13,8 +14,108 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def update(meal, items, user, date, currVal=0):
+    meal= clean(meal)
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    for item in items:
+        if currVal + int(item[1]) > 0:
+            query = f"Update {meal} set quantity=? where uid=? and date=? and item=?"
+            db.execute(query, (currVal + int(item[1]), user, date, item[0]))
+        else:
+            query = f"Delete from {meal} where uid=? and date=? and item=?"
+            db.execute(query, (user, date, item[0]))
+    conn.commit()
+
+def get_options(user):
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    db.execute("Select email, hash, name from users where id=?", (user,))
+    rec = db.fetchone()
+    opt = {
+        "email": rec[0],
+        "hash": rec[1],
+        "name": rec[2]
+    }
+    return opt
+
+def update_options(user, opts):
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    db.execute("Update users set email=?, hash=?, name=? where id=?", (opts["email"], opts["hash"], opts["name"], user))
+    conn.commit()
+    
+def get_goals(user):
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    db.execute("Select currWeight, goalWeight, calories from goals where uid=?", (user,))
+    rec = db.fetchone()
+    if not rec:
+        db.execute("Insert into goals values (?, ?, ?, ?)", (user, None, None, None))
+        conn.commit()
+        goals = {
+            "currWeight": None,
+            "goalWeight": None,
+            "calories": None
+        }
+    else:
+        goals = {
+            "currWeight": rec[0],
+            "goalWeight": rec[1],
+            "calories": rec[2]
+        }
+    return goals
+
+def update_goals(user, currWeight, goalWeight, calories):
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    db.execute("Update goals set currWeight=?, goalWeight=?, calories=? where uid=?", (currWeight, goalWeight, calories, user))
+    conn.commit()
+
+def add_items(meal, items, user, date):
+    meal= clean(meal)
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    for item in items:
+        db.execute(f"Select * from {meal} where item=? and date=? and uid=?;", (item[0], date, user))
+        existing = db.fetchone()
+        if existing:
+            update(meal, [item], user, date, currVal=existing[1])
+        else:
+            query = f"Insert into {meal} values (?, ?, ?, ?)"
+            db.execute(query, (item[0], item[1], user, date))
+    conn.commit()
+
+def get_entry(meal, user, date):
+    meal = clean(meal)
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    query = f"Select SUM(quantity), * from {meal} inner join recipes on {meal}.item=recipes.id where {meal}.uid=? and {meal}.date=? group by {meal}.item;"
+    db.execute(query, (user, date))
+    result = db.fetchall()
+    return result
+
+def get_total(result):
+    totals = {
+        "calories": 0,
+        "carbs": 0,
+        "protein": 0,
+        "fat": 0
+    }
+    for item in result:
+        totals["calories"] += round(item[0] * item[7], 2)
+        totals["carbs"] += round(item[0] * item[8], 2)
+        totals["protein"] += round(item[0] * item[9], 2)
+        totals["fat"] += round(item[0] * item[10], 2)
+    return totals
+
+def clean(string):
+    return ''.join(c for c in string if c.isalnum())
+
 def get_menu(date, meal):
-    url = f'http://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?date={date}&meal={meal}'
+    conn = sqlite3.connect("verifit.db")
+    db = conn.cursor()
+    url = f'http://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?date={date}&meal={meal}&type=30'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -24,27 +125,48 @@ def get_menu(date, meal):
     selector = CSSSelector("[href*='http\://www.foodpro.huds.harvard.edu/foodpro/item.asp?recipe=']")
     items = selector(site)
     menu=[]
+    recipeRegex = re.compile(r"recipe=(?P<recipe>\d+)&")
     nutritionRegex = re.compile(r"Serving Size:</b> (?P<quantity>.+?)(&nbsp;)(?P<unit>.+?)<br />.*<b>Calories:</b> (?P<calories>.+?)<br />.*?<b>Total Fat:</b> (?P<fat>.+?) g<br />.*?<b>Total Carbs:</b> (?P<carbs>.+?) g<br />.*?<b>Protein:</b> (?P<protein>.+?) g<br />", re.S)
-    #calServRegex = re.compile(r'Serving Size:</b>(?P<quantity>.+?)(&nbsp;)?(?P<unit>.+?)<br />\s*<b>Calories:</b>(?P<calories>.+?)<br />')
     for item in items:
         itemUrl = item.get('href')
         name = item.text.strip()
-        try:
-            itemResponse = requests.get(itemUrl)
-            itemResponse.raise_for_status()
-        except requests.RequestException:
-            return None
-        raw = itemResponse.text
-        matches = nutritionRegex.search(raw)
-        if not matches:
-            return None
-        info = matches.groupdict()
-        info['name'] = name
-        menu.append(info)
+        recipe = recipeRegex.search(itemUrl).group("recipe")
+        db.execute("Select * from recipes where id=?;", (int(recipe),))
+        recipeCheck = db.fetchone()
+        if not recipeCheck:
+            try:
+                itemResponse = requests.get(itemUrl)
+                itemResponse.raise_for_status()
+            except requests.RequestException:
+                print("ITEM REQUEST RETURNED NONE")
+                return None
+            raw = itemResponse.text
+            matches = nutritionRegex.search(raw)
+            if matches:
+                info = matches.groupdict()
+                info['name'] = name
+                info['id'] = recipe
+                menu.append(info)
+                db.execute("Insert into recipes values (?,?,?,?,?,?,?,?);", (recipe, name, matches.group("calories"), matches.group("carbs"), matches.group("protein"), matches.group("fat"), matches.group("quantity"), matches.group("unit")))
+                conn.commit()
+        else:
+            addition = {
+                "id": recipeCheck[0],
+                "name":recipeCheck[1],
+                "quantity":recipeCheck[6],
+                "unit":recipeCheck[7],
+                "calories":recipeCheck[2],
+                "carbs":recipeCheck[3],
+                "fat":recipeCheck[5],
+                "protein":recipeCheck[4],
+            }
+            menu.append(addition)
     return menu
 
 def reformat(date):
-    part = re.match(r"(\d{4})-(\d{2})-(\d{2})", date)
+    if not date:
+        return None
+    part = re.match(r"\d{2}(\d{2})-(\d{2})-(\d{2})", date)
     new = f"{part.group(2)}-{part.group(3)}-{part.group(1)}"
     return new
 
